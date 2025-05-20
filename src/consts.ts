72433083410d78dc2efa7effb1033c23914a7a45
이국진 <billlee297@gmail.com>
2025-05-20 22:08:05 +0900

    init: 프로젝트 생성 및 기본 세팅

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '이국진의 블로그';
+export const SITE_DESCRIPTION = '프론트엔드 개발자 이국진의 블로그입니다.';
